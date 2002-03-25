f980bd843f254c79f93c7ff7519977f4c0ef9b2a
Ben Webb <ben@salilab.org>
2002-03-25 16:51:42 +0000

    "Refresh" stock button added; initial attempt to emulate accelerators for stock buttons.

--- src/gtkport/gtkport.h
+++ src/gtkport/gtkport.h
@@ -760,11 +760,12 @@ void gtk_container_set_size(GtkWidget *widget, GtkAllocation *allocation);
 #define MB_IMMRETURN 16
 
 #ifndef HAVE_GLIB2
-#define GTK_STOCK_OK     _("OK")
-#define GTK_STOCK_CLOSE  _("Close")
-#define GTK_STOCK_CANCEL _("Cancel")
+#define GTK_STOCK_OK      _("_OK")
+#define GTK_STOCK_CLOSE   _("_Close")
+#define GTK_STOCK_CANCEL  _("_Cancel")
+#define GTK_STOCK_REFRESH _("_Refresh")
 
-#define gtk_button_new_from_stock gtk_button_new_with_label
+GtkWidget *gtk_button_new_from_stock(const gchar *label);
 #endif
 
 typedef struct _GtkUrl GtkUrl;
