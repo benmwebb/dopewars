c497edc3cf4c1441886db232819e9909f50b86ba
Vladimir Vrzić <vvrzic@gmail.com>
2023-07-19 18:54:45 +0200

    Use AX_WITH_CURSES macro to detect curses

--- src/cursesport/cursesport.h
+++ src/cursesport/cursesport.h
@@ -95,16 +95,18 @@ void endwin(void);
 #include <errno.h>
 
 /* Include a suitable curses-type library */
-#if HAVE_LIBNCURSES || defined(CURSES_HAVE_NCURSES_H)
-#include <ncurses.h>
-#elif HAVE_LIBCURSES || defined(CURSES_HAVE_CURSES_H)
-#include <curses.h>
-#elif defined(CURSES_HAVE_NCURSES_NCURSES_H)
-#include <ncurses/ncurses.h>
-#elif defined(CURSES_HAVE_NCURSES_CURSES_H)
-#include <ncurses/curses.h>
-#elif HAVE_LIBCUR_COLR
-#include <curses_colr/curses.h>
+#if defined HAVE_NCURSESW_CURSES_H
+#  include <ncursesw/curses.h>
+#elif defined HAVE_NCURSESW_H
+#  include <ncursesw.h>
+#elif defined HAVE_NCURSES_CURSES_H
+#  include <ncurses/curses.h>
+#elif defined HAVE_NCURSES_H
+#  include <ncurses.h>
+#elif defined HAVE_CURSES_H
+#  include <curses.h>
+#else
+#  error "SysV or X/Open-compatible Curses header file required"
 #endif
 
 #endif /* CYGWIN */
